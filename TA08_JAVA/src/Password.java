b96b7ddbd2a0827d56504efad44e1a9167d38c2a
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-19 01:16:31 +0200

    Creacion de constructores

--- TA08_JAVA/src/Password.java
+++ TA08_JAVA/src/Password.java
@@ -1,4 +1,74 @@
 
 public class Password {
 
+	
+	//Atributos:
+	
+		private int longitud;
+		private String contraseña;
+		
+		
+	//Constructores:
+		
+	//Constructor por defecto:
+	
+		public Password() {
+			
+			this.longitud =8;
+			this.contraseña = "";}
+		
+	//Constructor con longitud aplicada. Genera contraseña aleatoria con longitud definida
+		
+		public Password(int longitud, String contraseña) {
+			
+			this.longitud = longitud;
+			this.contraseña = contraseña;}
+
+		public int getLongitud() {
+			return longitud;
+		}
+
+		public void setLongitud(int longitud) {
+			this.longitud = longitud;
+		}
+
+		
+		public String getContraseña() {
+			return contraseña;
+		}
+
+		public void setContraseña(String contraseña) {
+			this.contraseña = contraseña;
+		}
+
+		@Override
+		public String toString() {
+			return "Password [longitud=" + longitud + ", contraseña=" + contraseña + "]";
+		}
+
+		
+		  
+		 public String generador() {
+			 String caracteres="ab157_?!";
+			 
+			 String num=Math.floor(Math.random()*(8+1);
+		
+			 return num;
+		 }
 }
+		
+
+
+
+		
+		
+		
+		 
+	
+		
+		
+	
+		
+		
+			
+
--- TA08_JAVA/src/Persona.java
+++ TA08_JAVA/src/Persona.java
@@ -1,4 +1,55 @@
 
 public class Persona {
 
+	//Crear atributos nombre, edad, DNI, sexo (hombre por defecto), peso y altura
+	
+	
+	//Atributos:
+	
+	private String nombre;
+	private int edad;
+	private String dni;
+	private String sexo;
+	private double peso;
+	private double altura;
+	
+	
+	//Constructores:
+	
+	//Constructor por defecto:
+	
+	public Persona(String dni) {
+		
+		this.nombre="";
+		this.edad=0;
+		this.dni=dni;
+		this.sexo="H";
+		this.peso=0;
+		this.altura=0;
+	}
+
+	//Constructor con 3 parametros (DNI, PESO Y ALTURA por defecto)
+	
+	public Persona(String nombre, int edad, String dni, String sexo) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dni = dni;
+		this.sexo = sexo;
+		this.peso = 0;
+		this.altura = 0;
+	}
+	
+	//Constructor con todos los atributos por parametro
+
+	public Persona(String nombre, int edad, String dni, String sexo, double peso, double altura) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dni = dni;
+		this.sexo = sexo;
+		this.peso = peso;
+		this.altura = altura;
+	}
+	
+	
 }
